3f5680fb4d8bfebda56d6b6db40fc34e8b06a46e
Alex <123456@qq.com>
2017-07-05 20:45:25 +0800

    factorial calculate

---
+++ chapter1_1/Ex20.java
@@ -0,0 +1,17 @@
+package chapter1_1;
+
+public class Ex20 {
+	public static double logFactorial(int N){
+		if (N == 0) return 0;
+		if (N == 1) return 0;
+		return logFactorial(N-1) + Math.log(N);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(logFactorial(0));
+		System.out.println(logFactorial(4));
+
+	}
+
+}
